71c725e1234d9927d9e095ec759a5f62a5c9e57e
Joanne <jeffiefeifei@gmail.com>
2017-01-26 12:42:14 -0800

    [fix] css when display on mobile

--- ScalablePress3.js
+++ ScalablePress3.js
@@ -1,15 +1,15 @@
 // get countDownDate in miliseconds:
-const countDownDate = new Date("Jan 28, 2018 12:24:00").getTime();
+var countDownDate = new Date("Jan 28, 2018 12:24:00").getTime();
 
-const countDownTimer = setInterval(() => {
+var countDownTimer = setInterval(function() {
   // Get diff between now an the count down date
-  const diff = countDownDate - new Date().getTime();
+  var diff = countDownDate - new Date().getTime();
 
   // calculations for days, hours, minutes and seconds
-  const days = Math.floor(diff / (1000 * 60 * 60 * 24));
-  const hours = Math.floor((diff % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-  const minutes = Math.floor((diff % (1000 * 60 * 60)) / (1000 * 60));
-  const seconds = Math.floor((diff % (1000 * 60)) / 1000);
+  var days = Math.floor(diff / (1000 * 60 * 60 * 24));
+  var hours = Math.floor((diff % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  var minutes = Math.floor((diff % (1000 * 60 * 60)) / (1000 * 60));
+  var seconds = Math.floor((diff % (1000 * 60)) / 1000);
 
   // Output the result in its own elements
   document.getElementById("days").innerHTML = days;
@@ -23,3 +23,30 @@ const countDownTimer = setInterval(() => {
     document.getElementById("box").innerHTML = "EXPIRED";
   }
 }, 1000);
+
+// Here is ES6 sytanx
+// get countDownDate in miliseconds:
+// const countDownDate = new Date("Jan 28, 2018 12:24:00").getTime();
+//
+// const countDownTimer = setInterval(() => {
+//   // Get diff between now an the count down date
+//   const diff = countDownDate - new Date().getTime();
+//
+//   // calculations for days, hours, minutes and seconds
+//   const days = Math.floor(diff / (1000 * 60 * 60 * 24));
+//   const hours = Math.floor((diff % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+//   const minutes = Math.floor((diff % (1000 * 60 * 60)) / (1000 * 60));
+//   const seconds = Math.floor((diff % (1000 * 60)) / 1000);
+//
+//   // Output the result in its own elements
+//   document.getElementById("days").innerHTML = days;
+//   document.getElementById("hours").innerHTML = hours;
+//   document.getElementById("minutes").innerHTML = minutes;
+//   document.getElementById("seconds").innerHTML = seconds;
+//
+//   // If the count down is over, write some text
+//   if (diff < 0) {
+//     clearInterval(countDownTimer);
+//     document.getElementById("box").innerHTML = "EXPIRED";
+//   }
+// }, 1000);
